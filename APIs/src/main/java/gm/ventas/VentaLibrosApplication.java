8e49cc9b9ab0122fa810dae6eaa90c784221d8dc
Ronald Lima <rl8506@gmail.com>
2025-12-14 20:58:45 -0300

    fix: cambiando nombre del proyecto

--- APIs/src/main/java/gm/ventas/VentaLibrosApplication.java
+++ APIs/src/main/java/gm/ventas/VentaLibrosApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class VentaLibrosMeliApplication {
+public class VentaLibrosApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VentaLibrosMeliApplication.class, args);
+        SpringApplication.run(VentaLibrosApplication.class, args);
     }
 
 }
